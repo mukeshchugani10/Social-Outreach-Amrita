b362704c2e8c438e50a35c3ec934e8db8314dcdf
Harshit Agarwal <harshitagar1907@gmail.com>
2019-08-20 00:52:30 +0530

    admin add event html

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,29 @@
 if (sessionStorage.getItem("token") != null) {
     window.location.pathname = '/../home'
 }
+var albumBucketName = 'outreachamrita';
+var bucketRegion = 'us-west-2';
+var IdentityPoolId = 'us-west-2:7adf791c-2ac9-4131-b587-f9960988dc6b';
 
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
+
+
+
+
+
+function addPhoto(){
+
+}
 
 $(document).ready(function () {
     // $('form').submit(false);
